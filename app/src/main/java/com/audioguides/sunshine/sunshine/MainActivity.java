ca7b192a399c48947b183376799c250f93ef92f8
jve0 <jve3112@gmail.com>
2016-03-19 01:15:05 +0000

    Lesson 2: added AsyncTask and weather API

--- app/src/main/java/com/audioguides/sunshine/sunshine/MainActivity.java
+++ app/src/main/java/com/audioguides/sunshine/sunshine/MainActivity.java
@@ -1,17 +1,10 @@
 package com.audioguides.sunshine.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -48,34 +44,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            ArrayList<String> weekForecast = new ArrayList<String>();
-            weekForecast.add("first data in list");
-            weekForecast.add("second data ");
-            weekForecast.add("third data type");
-            weekForecast.add("that's all folks!!");
-
-            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(getActivity(),
-                    R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
-
-            ListView mListView= (ListView)rootView.findViewById(R.id.listview_forecast);
-            mListView.setAdapter(mForecastAdapter);
-
-
-
-            return rootView;
-        }
-    }
 }
